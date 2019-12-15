64803711b9cbff5ffc72b4fc8b623a8597cf3b62
Douglas Brunal <frankithy@gmail.com>
2019-12-14 20:12:19 -0500

    reworked scoreboard in webui side

--- WebUI/scripts/main.js
+++ WebUI/scripts/main.js
@@ -5,9 +5,11 @@ function loadScoreBoardData(data){
 
     removeOldData();
 
+    var cars = ["Saab", "Volvo", "BMW"];
+
     for (let d = 0; d < data.length; d++){
         let player = data[d];
-        let td = '<tr><td>' + player.name + '</td><td>testweapon</td><td>' + player.score +  '</td></tr>';
+        let td = '<tr><td>' + player.name + '</td><td style="text-align: right;">testweapon</td><td style="text-align: right;">' + player.score +  '</td></tr>';
         let cont = document.getElementById("tablecontainer");
         cont.innerHTML += td;
     }
